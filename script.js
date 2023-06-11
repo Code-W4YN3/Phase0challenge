2c16c15967bbe17b8c3958dc9e22a1166120813e
Wayne Otido <otidowayne@gmail.com>
2023-06-11 16:48:00 +0300

    converting contacts to button

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+function myFunction() {
+  var x = document.getElementById("contacts");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
